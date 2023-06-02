3515dea2630f3e99ee9fb2571c4f7b8cfbaa7a39
Moesha-Githae <moesha.githae@student.moringaschool.com>
2023-06-02 08:48:25 +0300

    complete

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
-end
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{character.name}-#{character.show.name}"
+  end
+
+  def list_roles
+    self.characters.map do |character|
+      # binding.pry
+      "#{character.name} - #{character.show.name}"
+    end
+  end
+  end
+
+
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,8 @@
 class Network < ActiveRecord::Base
+  has_many :shows
+
+  def sorry
+    "were sorry about passiong on John Mulaney's pilot"
+  end
   
 end
